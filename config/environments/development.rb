02dcdad2720b6ba2daf4dfbe1bc8a51b2f976ed6
Tomohiko Takahashi <takahashi@tomohiko.io>
2021-09-19 06:27:32 +0000

    add host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "code.tomohiko.io"
+  config.hosts << "58ecb96a1c1c4eec90d9bb399fd1d74a.vfs.cloud9.us-east-1.amazonaws.com"
 end
