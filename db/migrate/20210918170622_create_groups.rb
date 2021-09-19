fb66386c9ed2b6c5301a48abccbec3f11c040c55
Tomohiko Takahashi <takahashi@tomohiko.io>
2021-09-19 07:19:13 +0000

    fix typo

--- db/migrate/20210918170622_create_groups.rb
+++ db/migrate/20210918170622_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[6.1]
   def change
     create_table :groups do |t|
       t.string :name
-      t.references :Tflow, null: false, foreign_key: true
+      t.references :tflow, null: false, foreign_key: true
 
       t.timestamps
     end
