957dc035f35ed2e2b48a8034c7bc5aa07fd49ac2
Tomohiko Takahashi <takahashi@tomohiko.io>
2021-09-20 11:36:09 +0000

    flowを更新するとtaskがINSERTされてしまうがとりあえずコミット

--- app/controllers/flows_controller.rb
+++ app/controllers/flows_controller.rb
@@ -1,4 +1,5 @@
 class FlowsController < ApplicationController
+  # TODO: show, editにset_flowメソッドを追加
   def index
     @flow = Tflow.all().eager_load(:task)
   end
@@ -11,8 +12,36 @@ class FlowsController < ApplicationController
   end
 
   def edit
+    @flow = Tflow.find(params['id'])
+  end
+  
+  def create
+  end
+  
+  def update
+    respond_to do |format|
+      @flow = Tflow.find(params[:id])
+      if @flow.update(tflow_params)
+        format.html { redirect_to flow_path(@flow.id) }
+        format.json { render :show, status: :ok, location: @flow }
+      else
+        format.html { render :edit }
+        format.json { render json: @flow.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+  end
+  
+  def tflow_params
+    params.require(:tflow).permit(
+      :name,
+      :description,
+      task_attributes: [
+        :name,
+        :description
+      ]
+    )
   end
 end
--- app/models/tflow.rb
+++ app/models/tflow.rb
@@ -1,3 +1,4 @@
 class Tflow < ApplicationRecord
-  has_many :task
+  has_many :task, dependent: :destroy
+  accepts_nested_attributes_for :task, allow_destroy: true
 end
